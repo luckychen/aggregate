6b5db209903a2b3920c25f3f4b46b988c244eb31
chong chen <chong.chen@unlv.edu>
2019-06-11 12:46:34 -0700

    first version

--- solution.cc
+++ solution.cc
@@ -5,11 +5,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 #include <list>
+#include <unistd.h>
 using namespace std;
 
 typedef struct _node{
-	unsigned int duplicated; 
 	unsigned int groupID;
 	vector<string> data;
 }node;
@@ -17,160 +18,258 @@ typedef struct _node{
 inline string extractSeq(string data){
 	unsigned int firstL = data.find_first_not_of(' ');
 	auto trimed = data.substr(firstL, data.size());
-	unsigned int splitPo = trimed.find(' '); 
+	unsigned int splitPo = min(trimed.find_first_of('\t'), trimed.find_first_of(' ')); 
 	return trimed.substr(0, splitPo - 1);
 }
 
 
-class arregatedData {
+class aggregatedData{
 	private:
 		unsigned int globalGroupCount;
-		unsigned int globalUniqueCount;
 		unsigned int dataLine;
 		string fileName; 
-		map<string, node*>& originData;
-		vector<node*> firstHalfNodeVec;
-		vector<node*> secondHalfNodeVec;	
-		vector<map*> halfMapVec; 
+		string inputFileHead;
+		map<string, node*> originData;
 		node *firstHalfNode, *secondHalfNode;
-		map<string, node*> *secondHalfMap; 
-		map<string, node*> *firstHalfMap;
-		void buildHalfMap(originData);
+		map<string, node*> secondHalfMap; 
+		map<string, node*> firstHalfMap;
+		vector<string> *aggregatedVec;
+		vector<string> splitHalf(const string &input);
+		void buildHalfMap();
+		bool oneCharDiff(string &S1, string &S2);
 	public:
-	 
-		void arregatedData(string fileName_):fileName(fileName_){
+		aggregatedData(string fileName_):fileName(fileName_){
 			globalGroupCount = 0;
 			dataLine = 0;
-			ifstream sampleRead("../all_sample.reads");
+			string line;
+			ifstream sampleRead;
 			//read the file and put every unique seq into a map
-			if(sampleRead.open()){
-				getline(sampleRead, line);//head line
-				while(getline(sampleRead, line)){
-					dataLine++;
-					extractSeq(line, seq);		
-					auto origIt = originData.find(seq);
-					if(origIt != originData.end()){
-						readData[seq]->data.push_back(line);
-					} else {
-						globalGroupCount +=1;
-						temp = new node;
-						dataPointBuf.push_back(temp);
-						readData[seq] = dataPointBuf.back();	
+			sampleRead.open("../all_sample.reads");
+			getline(sampleRead, inputFileHead);//head line
+			while(getline(sampleRead, line)){
+				dataLine++;
+				auto seq = extractSeq(line);		
+				auto origIt = originData.find(seq);
+				if(origIt != originData.end()){
+					if(originData[seq]->groupID == 0){
+						globalGroupCount += 1;
+						originData[seq]->groupID = globalGroupCount;
 					}
-				}		
-			} else {
-				puts("file open failed");
-			}
+					originData[seq]->data.push_back(line);
+				} else {
+					auto temp = new node;
+					temp->groupID = 0;
+					temp->data.push_back(line);
+					originData[seq] = temp;	
+				}
+			}		
+			
+			buildHalfMap();
 		}
-		void arregatedExecution();
-		void ~arregatedData(){
-			for(auto const &i : firstHalfNodeVec){
-				delete(*i);//all first half nodes were deleted 
-			}
-			for(auto const &i : secondHalfNodeVec){
-				delete(*i);//all second half nodes were deleted
-			}
+		void writeResult(string file);
+		void arrangeGroupId();
+		~aggregatedData(){
 			for(auto const &i : originData){
-				delete(i->second);//all data nodes were deleted
+				delete(i.second);//all data nodes were deleted
+			}
+			for(auto const &i : firstHalfMap){
+				delete(i.second);//all data nodes were deleted
+			}
+			for(auto const &i : secondHalfMap){
+				delete(i.second);//all data nodes were deleted
 			}
 		}
 
-}
+};
 
+vector<string> aggregatedData::splitHalf(const string &input){
+	vector<string> result(2);
+	result[0] = input.substr(0, input.size()/2);
+	result[1] = input.substr(input.size()/2, input.size() - input.size()/2);	
+	return result;
+}
 void aggregatedData::buildHalfMap(){
-	firstHalfMap = new map<string, node*>;
-	secondHalfMap = new map<string, node*>;
 	vector<string> dividedString;	
 	for(auto &i : originData){
 		dividedString = splitHalf(i.first);
-		if(firstHalfMap.find(dividedString[0])){
-			firstHalfMap[dividedString[0]]->duplicated += 1;
-			firstHalfMap[dividedString[0]]->data.push_back[dividedString[1]];
+		auto i1 = firstHalfMap.find(dividedString[0]);
+		if(i1 != firstHalfMap.end()){
+			firstHalfMap[dividedString[0]]->data.push_back(dividedString[1]);
 		} else {
-			firstHalfNode = new class node; 
-			firstHalfNode->duplicated = 0;
+			firstHalfNode = new node; 
 			firstHalfNode->data.push_back(dividedString[1]);
 			firstHalfMap[dividedString[0]] = firstHalfNode;
-			firstHalfNodeVec.push_back(node);
 		}
-		if(secondHalfMap.find(dividedString[1])){
-			secondHalfMap[dividedString[1]]->duplicated+=1;
-			secondHalfMap[dividedString[1]]->data.push_back[dividedString[0]];
+		auto i2 = secondHalfMap.find(dividedString[1]);
+		if(i2 != secondHalfMap.end()){
+			secondHalfMap[dividedString[1]]->data.push_back(dividedString[0]);
 		} else {
-			secondHalf = new class node;
-			secondHalf->duplicated = 0;
-			secondHalf->data.push_back(dividedString[0]);
-			secondHalfMap[dividedString[1]] = secondHalf;
-			secondHalfNodeVec.push_back(secondHalf);
+			secondHalfNode = new node;
+			secondHalfNode->data.push_back(dividedString[0]);
+			secondHalfMap[dividedString[1]] = secondHalfNode;
 		}
 	}
 }
 
-void aggregatedData::arrangeGroupId(map<string, node*>* firstHalfMap, map<string, node*>* secondHalfMap, 
-		const unsigned int len){
+void aggregatedData::arrangeGroupId(){
 
 	vector<string> dividedString;
 	for(auto &origIt : originData){
 		dividedString = splitHalf(origIt.first);
 		auto firstHalfKey = dividedString[0];
 		auto secondHalfKey = dividedString[1];
-		string origKey;
-		auto it = firstHalfMap.find(firstHalf); 
-		auto it2 = secondHalfMap.firstHalf(secondHalf);
+		string origKey = origIt.first;
+	
+		auto it = firstHalfMap.find(firstHalfKey); 
+		auto it2 = secondHalfMap.find(secondHalfKey);
 		if(it != firstHalfMap.end()){
-			for(auto &hitString : it->data){
+			for(auto &hitString : it->second->data){
 				if(oneCharDiff(secondHalfKey, hitString) == true){
-					hitKey = firstHalf.append(hitString);	
-					if(origIt.groupID == 0 && originData[hitKey].groupID == 0){
+					string hitKey = firstHalfKey;	
+					hitKey.append(hitString);
+					if(origIt.second->groupID != 0 && originData[hitKey]->groupID != 0){
+						continue;
+					} else if(origIt.second->groupID == 0 && originData[hitKey]->groupID == 0){
 						globalGroupCount += 1;
-						originData[hitKey].groupID =  originData.groupID = globalGroupCount;
+						originData[hitKey]->groupID =  originData[origKey]->groupID = globalGroupCount;
 					} else {
-						originData.groupID == 0 ? originData[hitKey].groupID = originData.groupID 
-							: originData.groupID = originData[hitKey].groupID;
+						originData[hitKey]->groupID == 0 ? originData[hitKey]->groupID = originData[origKey]->groupID 
+							: originData[origKey]->groupID = originData[hitKey]->groupID;
 					}
 				} 
 			}		
 		}
 		if(it2 != secondHalfMap.end()){
-			for(auto &hitString : it->data){
+			for(auto &hitString : it2->second->data){
 				if(oneCharDiff(firstHalfKey, hitString) == true){
-					hitKey = hitString.append(secondHalfKey);	
-					if(origIt.groupID == 0 && originData[hitKey].groupID == 0){
+					string hitKey = hitString;	
+					hitKey.append(secondHalfKey);
+					if(origIt.second->groupID != 0 && originData[hitKey]->groupID != 0){
+						continue;
+					}else if(origIt.second->groupID == 0 && originData[hitKey]->groupID == 0){
 						globalGroupCount += 1;
-						originData[hitKey].groupID =  originData.groupID = globalGroupCount;
+						originData[hitKey]->groupID = originData[origKey]->groupID = globalGroupCount;
 					} else {
-						originData.groupID == 0 ? originData[hitKey].groupID = originData.groupID 
-							: originData.groupID = originData[hitKey].groupID;
+						originData[hitKey]->groupID == 0 ? originData[hitKey]->groupID = originData[origKey]->groupID 
+							: originData[origKey]->groupID = originData[hitKey]->groupID;
 					}
 				} 
+			}
+		}
+		auto tempKey = origKey.substr(1, origKey.size() - 1); 
+		auto it3 = originData.find(tempKey);
+		if(it3 != originData.end()){
+			if(it3->second->groupID != 0 && origIt.second->groupID != 0){
+				continue;
+			}else if(it3->second->groupID == 0 && origIt.second->groupID == 0){
+				globalGroupCount += 1;
+				originData[tempKey]->groupID = originData[origKey]->groupID = globalGroupCount;
+			} else {
+				originData[tempKey]->groupID == 0 ? originData[tempKey]->groupID = originData[origKey]->groupID 
+					: originData[origKey]->groupID = originData[tempKey]->groupID;
+			}
+		}
+		
+		tempKey = origKey.substr(0, origKey.size() - 1);
+		auto it4 = originData.find(tempKey);
+		if(it4 != originData.end()){
+			if(it4->second->groupID != 0 && origIt.second->groupID != 0){
+				continue;
+			} else if(it4->second->groupID == 0 && origIt.second->groupID == 0){
+				globalGroupCount += 1;
+				originData[tempKey]->groupID = originData[origKey]->groupID = globalGroupCount;
+			} else {
+				originData[tempKey]->groupID == 0 ? originData[tempKey]->groupID = originData[origKey]->groupID 
+					: originData[origKey]->groupID = originData[tempKey]->groupID;
 			}
 		}
 
 	}
 }
 
-void arregatedData::arregatedExecution(){
-	for(unsigned int i = minSeqLen; i <= maxSeqLen; ++i){
-		this->arrangeGroupId(halfMapVec[(i - minSeqLen)*2], halfMapVec[(i - minSeqLen)*2 + 1], i);
+bool aggregatedData::oneCharDiff(string& s1, string& s2){
+	unsigned int diff;
+	if(s1.length() != s2.length())
+		return false;
+	diff = 0;
+	for(unsigned int i = 0; i < s1.length(); ++i){
+		if(s1.at(i) != s2.at(i)){
+			diff += 1;
+		}
+		if(diff > 1){
+			return false;
+		}
+	}
+	if(diff == 1)
+		return true;
+
+	return false;
+}
+void aggregatedData::writeResult(string outFileName){
+	vector<vector<node*>> groupIdVec;
+	for(unsigned int i = 0; i <= globalGroupCount; ++i){
+		groupIdVec.push_back({});
 	}	
+	ofstream ofs(outFileName, ofstream::out);
+	for(auto &it : originData){
+		if(it.second->groupID != 0){
+			groupIdVec[(it.second)->groupID].push_back(it.second);	
+		}
+	}
+	ofs << "groupID\t" + inputFileHead << endl;
+	for(unsigned int i = 0; i < groupIdVec.size(); ++i){
+		for(auto &it : groupIdVec[i]){
+			for(unsigned int j = 0; j < it->data.size(); ++j){
+				ofs<<to_string(i) + '\t' + it->data[j] << endl;
+			}
+		}
+	}
+	ofs.close();
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
 	//read file
 	string line;	
 	string seq;
 	map<string, node*> readData;
-	vector<node*> dataPointBuf;
 	node* temp;
-	
-	//find the similar seq with hamin distance less than 1
-	class arregatedData(readData, minSeqLen, maxSeqLen);	
-	arregatedData.store("all_samples_arregate");	
+	char fileName[100];
+	char resultName[100];
+	int oc;
+	while ((oc = getopt(argc, argv, "i:o:")) != -1) {	
 
-	for(auto &i : dataPointBuf){
-		delete i;
+		switch (oc) {
+			case 'i':
+				/* input data file name */
+				sprintf(fileName, "%s", optarg);
+				break;
+			case 'o':
+				/* output result file name */
+				sprintf(resultName, "%s", optarg);
+				break;
+			case ':':
+				printf("missing arguments\n");
+				exit(0);
+				break;
+			case '?':
+				printf("unrecongnized option\n");
+				break;
+			default:
+				printf("option/arguments error!\n");       /* error handling, see text */
+				exit(0);
+
+		}
+	}
+	if (fileName[0] == '\0' || resultName[0] == '\0'){
+		cout<<"input file name or reslut file name missing"<<endl;
+		cout<<"execute it as ./test -i [input file name] -o [output file name]"<<endl;
+		exit(0);
 	}
 
-    return 0;
+	class aggregatedData aggregatedData_(fileName);
+	aggregatedData_.arrangeGroupId();	
+	aggregatedData_.writeResult(resultName);
+
+	return 0;
 }
